c760409c70bea0817d8939652039f5c1417a0e07
Paul Hernández <mpaul.hernandez@gmail.com>
2020-02-12 22:40:57 -0600

    Define ordering scopes

--- app/models/city_temperature.rb
+++ app/models/city_temperature.rb
@@ -1,2 +1,4 @@
 class CityTemperature < ApplicationRecord
+  scope :order_by_temperature, ->(direction) { order(temperature: direction) }
+  scope :order_by_created_at, ->(direction) { order(created_at: direction) }
 end
