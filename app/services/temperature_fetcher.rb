cee3a989d6e7a645fd5fd7ba4a39590c2dd51298
Paul Hernández <mpaul.hernandez@gmail.com>
2020-02-12 21:53:49 -0600

    Implement fetching logic in TemperatureFetcher

--- app/services/temperature_fetcher.rb
+++ app/services/temperature_fetcher.rb
@@ -1,10 +1,29 @@
 class TemperatureFetcher
-  attr_reader :city_id
-  def initialize()
-    @city_id = 3995465
+  attr_reader :city_id, :unit, :app_id, :api_url
+  def initialize(city_id, unit = "metric")
+    @city_id = city_id
+    @unit    = unit
+    @app_id  = ENV["OPENWEATHER_APP_ID"]
+    @api_url = ENV["OPENWEATHER_ENDPOINT"]
   end
 
   def call()
-    # logic for calling openweather API
+    begin
+      url      = request_uri
+      http     = Net::HTTP.new(url.host, url.port)
+      request  = Net::HTTP::Get.new(url)
+
+      response = http.request(request)
+
+      JSON.parse(response.body)
+    rescue
+      nil
+    end
+  end
+
+  private
+
+  def request_uri
+    URI("http://#{api_url}?id=#{city_id}&appid=#{app_id}&units=#{unit}")
   end
 end
