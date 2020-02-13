2d89c25d60767f48c46aad16a4c2ba2391571792
Paul Hernández <mpaul.hernandez@gmail.com>
2020-02-12 21:17:10 -0600

    Run db migrations

---
+++ db/schema.rb
@@ -0,0 +1,24 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_02_13_031344) do
+
+  create_table "city_temperatures", force: :cascade do |t|
+    t.string "country"
+    t.string "name"
+    t.float "temperature"
+    t.string "unit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
